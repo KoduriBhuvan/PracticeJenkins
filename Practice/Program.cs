30dd28b92ce30e02b7c9e7da97c4a534b5c4c418
Koduri Bhuvan <bhuvankoduri4800@gmail.com>
2023-07-20 12:04:56 +0530

    first commit

---
+++ Practice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        void greet()
+        {
+            var name = Console.ReadLine();
+            Console.WriteLine("hi  " + name);
+        }
+        int add(int x,int y)
+        {
+            return x + y;
+        }
+        greet();
+        Console.WriteLine(add(3, 5));
+    }
+}
